4d26a77ce45b5b9e15c7d6733584007f4efafd82
lpicciri <lpicciri@student.42roma.it>
2024-07-06 00:15:44 +0200

    protezioni wrong cat

--- CPP04/ex00/WrongCat.hpp
+++ CPP04/ex00/WrongCat.hpp
@@ -6,10 +6,12 @@
 /*   By: luca <luca@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:55:48 by luca              #+#    #+#             */
-/*   Updated: 2024/06/18 17:58:24 by luca             ###   ########.fr       */
+/*   Updated: 2024/07/06 00:15:26 by luca             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WrongCat_HPP
+#define WrongCat_HPP
 #include "WrongAnimal.hpp"
 
 class WrongCat : public WrongAnimal
@@ -20,3 +22,5 @@ class WrongCat : public WrongAnimal
 		WrongCat& operator=(const WrongCat &wrongcat);
 		void makeSound() const ;
 };
+
+#endif
