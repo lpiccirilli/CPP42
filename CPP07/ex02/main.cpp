0cbbe68002d676881471a5a760cd25ef0adb9db1
lpiccirilli <nuclei-secchiata.0k@icloud.com>
2025-01-23 14:11:53 +0100

    corretto fino a CPP07 ex01

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: luca <luca@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 17:40:47 by luca              #+#    #+#             */
-/*   Updated: 2024/08/14 17:41:53 by luca             ###   ########.fr       */
+/*   Updated: 2025/01/23 14:10:57 by luca             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int main(int, char**)
 {
 	try
 	{
+		int *a = new int();
+		std::cout << *a << std::endl;
 		Array <int>intArray(100);
 		Array <int>copia(intArray);
 		for (int i = 0; i < 100; i++)
 			intArray[i] = i;
-		copia[-2] = 42;
 		std::cout << copia[2] << std::endl;
 		std::cout << intArray[2] << std::endl;
 		std::cout << intArray.size() << std::endl;
